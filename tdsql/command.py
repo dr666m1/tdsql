8c8d509c86729439cc39f5fc402bc5619975fd9b
dr666m1 <skndr666m1@gmail.com>
2022-05-22 11:16:12 +0900

    Simplify _detect_test_config
    callable(f.type) is useless here
    because even Literal['foo'] is callable.

--- tdsql/command.py
+++ tdsql/command.py
@@ -29,10 +29,12 @@ def _detect_test_config(yamldict: dict[str, Any]) -> TdsqlTestConfig:
     for f in fields(TdsqlTestConfig):
         val = yamldict.get(f.name)
         if val is None:
-            continue
-        if f.type == type(val):
+            pass
+
+        elif f.type == type(val):
             kwargs[f.name] = val
-        elif callable(f.type):
+
+        else:
             try:
                 kwargs[f.name] = f.type(val)
             except ValueError:
