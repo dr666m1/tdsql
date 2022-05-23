bbb4b6a296095301da700366f83236c93c53927d
dr666m1 <skndr666m1@gmail.com>
2022-05-23 20:43:46 +0900

    Improve entrypoint

--- tdsql/__main__.py
+++ tdsql/__main__.py
@@ -1,6 +1,18 @@
 from pathlib import Path
 
 from tdsql.command import run
+from tdsql.logger import logger
 
 if __name__ == "__main__":
-    run(Path("tdsql.yaml"))
+    yamlpath = Path("tdsql.yaml")
+    ymlpath = Path("tdsql.yml")
+
+    if yamlpath.is_file():
+        run(yamlpath)
+
+    elif ymlpath.is_file():
+        run(ymlpath)
+
+    else:
+        logger.error("tdsql.yaml is not found")
+        exit(code=1)
--- tdsql/command.py
+++ tdsql/command.py
@@ -155,3 +155,18 @@ def _is_equal(actual: Any, expected: Any, acceptable_error: float) -> bool:
     else:
         res = actual == expected
         return res
+
+
+if __name__ == "__main__":
+    yamlpath = Path("tdsql.yaml")
+    ymlpath = Path("tdsql.yml")
+
+    if yamlpath.is_file():
+        run(yamlpath)
+
+    elif ymlpath.is_file():
+        run(ymlpath)
+
+    else:
+        logger.error("tdsql.yaml is not found")
+        exit(code=1)
